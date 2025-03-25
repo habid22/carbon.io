8650f71b10508e7c2f94b4f7d6d21fe5566b4624
habid22 <habid22@uwo.ca>
2025-03-24 23:09:27 -0400

    Add starry background and shooting stars components; update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@
 
 import GlobeDemo from "@/components/ui/globe-demo";
 
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/star-background";
+import { Globe } from "@/components/globe";
+
+
 export default function Home() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <GlobeDemo />
+    
+      <GlobeDemo/>
+      <ShootingStars />
+      <StarsBackground />
+
     </div>
   );
 }
--- components/ui/globe-demo.tsx
+++ components/ui/globe-demo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 
-const World = dynamic(() => import("@/components/ui/globe").then((m) => m.World), {
+const World = dynamic(() => import("@/components/globe").then((m) => m.World), {
   ssr: false,
 });
 
